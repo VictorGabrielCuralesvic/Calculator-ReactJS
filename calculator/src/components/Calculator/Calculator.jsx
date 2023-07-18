0a3419d8ba01085284f42eb6e7df4fc8cc050308
Nome <miranhaomi@gmail.com>
2023-07-17 22:02:30 -0300

    Atualizando o Calculator.jsx (III)
    Atualizando o componente para adicionar a parte de renderização do componente Calculator.jsx, retornando os botões e campo de entrada feitos anteriormente em outros componentes menores.

--- calculator/src/components/Calculator/Calculator.jsx
+++ calculator/src/components/Calculator/Calculator.jsx
@@ -6,6 +6,7 @@ import "../../Style/calculator.css";
 
 const Calculator = () => {
   const [val, setVal] = useState("");
+  const [memory, setMemory] = useState(0);
 
   const handleInputChange = (value) => {
     setVal(value);
@@ -31,3 +32,46 @@ const Calculator = () => {
     }
   };
 
+  return (
+    <div className="calculator">
+      <div>
+        <div>
+          <h1>CALCULATOR</h1>
+        </div>
+      </div>
+      <div>
+        <div>
+          <InputField value={val} onChange={handleInputChange} />
+          <div className="button-container">
+            <NumberButton value="1" onClick={() => handleButtonClick("1")} />
+            <NumberButton value="2" onClick={() => handleButtonClick("2")} />
+            <NumberButton value="3" onClick={() => handleButtonClick("3")} />
+            <OperatorButton value="CE" onClick={handleBackspace} label="CE" />
+
+            <NumberButton value="4" onClick={() => handleButtonClick("4")} />
+            <NumberButton value="5" onClick={() => handleButtonClick("5")} />
+            <NumberButton value="6" onClick={() => handleButtonClick("6")} />
+            <OperatorButton value="+" onClick={() => handleButtonClick("+")} label="+" />
+
+            <NumberButton value="7" onClick={() => handleButtonClick("7")} />
+            <NumberButton value="8" onClick={() => handleButtonClick("8")} />
+            <NumberButton value="9" onClick={() => handleButtonClick("9")} />
+            <OperatorButton value="*" onClick={() => handleButtonClick("*")} label="x" />
+
+            <NumberButton value="." onClick={() => handleButtonClick(".")} />
+            <NumberButton value="0" onClick={() => handleButtonClick("0")} />
+            <OperatorButton value="=" onClick={handleCalculate} label="=" />
+            <OperatorButton value="/" onClick={() => handleButtonClick("/")} label="/" />
+
+            <OperatorButton value="MC" onClick={handleMemoryClear} label="MC" />
+            <OperatorButton value="MR" onClick={handleMemoryRecall} label="MR" />
+            <OperatorButton value="M+" onClick={handleMemoryAdd} label="M+" />
+            <OperatorButton value="M-" onClick={handleMemorySubtract} label="M-" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
