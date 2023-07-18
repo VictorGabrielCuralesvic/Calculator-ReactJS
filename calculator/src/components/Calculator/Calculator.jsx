5e6b7d4b3a760c8046046907ae8e7427a8650284
Nome <miranhaomi@gmail.com>
2023-07-17 21:29:19 -0300

    Criando o componente Calculator.jsx
    Elaborando o componente Calculator.jsx responsável por fazer as operações de fato e receber os componentes filhos e renderizar estes para o usuário no App.js.

---
+++ calculator/src/components/Calculator/Calculator.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import InputField from "../InputField/InputField";
+import OperatorButton from "../OperatorButton/OperatorButton";
+import NumberButton from "../NumberButton/NumberButton";
+import "../../Style/calculator.css";
+
+const Calculator = () => {
